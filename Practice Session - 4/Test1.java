b03a3c59990a0d28f739305562035d20ccb328fe
madhusarode <100706903+Madhusarode@users.noreply.github.com>
2024-01-04 16:24:39 +0530

    Add files via upload
    java

---
+++ Practice Session - 4/Test1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Test1{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if(num==0){
+            System.out.println("zero");
+        }
+        else if(num > 0){
+            System.out.println("Positive");
+        }
+        else{
+            System.out.println("Negative"
+            
+            );
+        }
+    }
+}
+
